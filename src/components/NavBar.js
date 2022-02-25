a019e0012e4557c7a2c669364e4832aeafa100c7
Brian Arnold <b.arnold09@gmail.com>
2022-02-25 11:47:30 -0500

    commit tree 150439a07d93987cac33aa5432a7af2429a3dce2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,26 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+           <a href="#home" >{links[0]}</a>
+           <a href="#about" >{links[1]}</a>
+           <a href="#projects" >{links[2]}</a>
+    </nav>
+  );
 }
 
 export default NavBar;
+
+// alternative for code staring from line 6?:
+  // renders three <a> elements
+  // displays the correct text for each <a> element
+  // each <a> element has the correct href attribute
+
+//   const linkLinks = links.map((link)=>{
+//     return <a key={link} href={link}>{link}</a>
+//   });
+//     return(
+//       <nav>{linkLinks}</nav>
+//  )
+// return <nav>
